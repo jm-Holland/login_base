41cf441a0c3ae8f3fba35fb13bb8fd1b0e26d6c5
illaweb35 <illaweb35@gmail.com>
2017-12-29 19:02:37 +0100

    ModifMineures

--- inc/footer.php
+++ inc/footer.php
@@ -15,5 +15,7 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
 <script src="js/menu_bulma.js" charset="utf-8"></script>
+<script src="js/top.js" charset="utf-8"></script>
+<a href="#" title="Haut de page" class="scrollup"><i class="fa fa-arrow-up"></i></a>
 </body>
 </html>
